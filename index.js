ca492737ac041962e998809d544055e935c2f83d
FIRST_NAME LAST_NAME <aarthikannan2103@gmail.com>
2021-04-11 15:17:43 +0530

    resp.send

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app
   .get(async (req, res) => {
     try {
       const users = await User.find();
-      res.json(users);
+      res.send(users);
     } catch (err) {
       res.send(err);
     }
